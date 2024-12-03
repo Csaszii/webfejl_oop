dbc3d6d83498e90a216014659986448f970cebda
Csaszii <csaszaroliver83@gmail.com>
2024-12-03 16:03:09 +0100

    kesz

---
+++ 11.29_gomszab/main.js
@@ -0,0 +1,43 @@
+class Player {
+    constructor(nickname) {
+        this.nickname = nickname;
+        this.playedMatches = 0;
+    }
+    play() {
+        this.playedMatches++;
+    }
+    getTierLevel() {
+        if (this.playedMatches <= 3) {
+            return "A";
+        }
+        else if (this.playedMatches > 3 && this.playedMatches <= 6) {
+            return "B";
+        }
+        else {
+            return "C";
+        }
+    }
+}
+
+function Person(name) {
+    this.name = name;
+}
+
+Person.prototype.getName = function() {
+    return this.name;
+}
+
+function Student(name, school) {
+    Person.call(this, name);
+    this.school = school;
+}
+
+Object.setPrototypeOf(Student, Person);
+
+const student = new Student("Jani", "ASD")
+console.log(student)
+
+const gomszab = new Player("gomszab");
+gomszab.play()
+console.log(gomszab);
+console.log(gomszab.getTierLevel());
